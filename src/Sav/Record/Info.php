afc76dba9b5c812f2968d5d54e8b40f83ecd0f4e
flobee <flobee@debian>
2017-11-17 00:39:59 +0100

    Improves CS

--- src/Sav/Record/Info.php
+++ src/Sav/Record/Info.php
@@ -5,38 +5,51 @@ namespace SPSS\Sav\Record;
 use SPSS\Buffer;
 use SPSS\Sav\Record;
 
-abstract class Info extends Record
+
+abstract class Info
+    extends Record
 {
     const TYPE = 7;
     const SUBTYPE = 0;
 
     /**
-     * @var int Size of each piece of data in the data part, in bytes
+     * Size of each piece of data in the data part, in bytes.
+     *
+     * @var int
      */
     protected $dataSize = 1;
 
     /**
-     * @var int Number of pieces of data in the data part
+     * Number of pieces of data in the data part.
+     *
+     * @var int
      */
     protected $dataCount = 0;
 
+
     /**
+     * Read from buffer.
+     *
      * @param Buffer $buffer
      */
-    public function read(Buffer $buffer)
+    public function read( Buffer $buffer )
     {
         $this->dataSize = $buffer->readInt();
         $this->dataCount = $buffer->readInt();
     }
 
+
     /**
+     * Write to buffer.
+     *
      * @param Buffer $buffer
      */
-    public function write(Buffer $buffer)
+    public function write( Buffer $buffer )
     {
-        $buffer->writeInt(self::TYPE);
-        $buffer->writeInt(static::SUBTYPE);
-        $buffer->writeInt($this->dataSize);
-        $buffer->writeInt($this->dataCount);
+        $buffer->writeInt( self::TYPE );
+        $buffer->writeInt( static::SUBTYPE );
+        $buffer->writeInt( $this->dataSize );
+        $buffer->writeInt( $this->dataCount );
     }
+
 }
