6c0233995b58a6bc3ab57c821fb542f664023e83
flobee <flobee@debian>
2017-11-17 00:58:41 +0100

    Improves CS

--- src/Sav/Record.php
+++ src/Sav/Record.php
@@ -4,44 +4,54 @@ namespace SPSS\Sav;
 
 use SPSS\Buffer;
 
+
 abstract class Record
 {
     /**
-     * @var int Record type code
+     * Record type code.
+     *
+     * @var int
      */
     const TYPE = 0;
 
+
     /**
      * Record constructor.
+     *
      * @param array $data
      */
-    public function __construct($data = [])
+    public function __construct( $data = array() )
     {
-        foreach ($data as $key => $value) {
+        foreach ( $data as $key => $value ) {
             $this->{$key} = $value;
         }
     }
 
+
     /**
      * @param Buffer $buffer
+     *
      * @return static
      */
-    public static function fill(Buffer $buffer)
+    public static function fill( Buffer $buffer )
     {
         $record = new static();
-        $record->read($buffer);
+        $record->read( $buffer );
         return $record;
     }
 
-    /**
-     * @param Buffer $buffer
-     * @return void
-     */
-    abstract public function read(Buffer $buffer);
 
     /**
      * @param Buffer $buffer
      * @return void
      */
-    abstract public function write(Buffer $buffer);
+    abstract public function read( Buffer $buffer );
+
+
+    /**
+     * @param Buffer $buffer
+     * @return void
+     */
+    abstract public function write( Buffer $buffer );
+
 }
