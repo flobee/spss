68b7fe0b3e031362b89a8078c4d0d30716f7d8b2
flobee <flobee.code@gmail.com>
2020-09-08 19:38:11 +0200

    Updates tests/phpbin.sh

--- tests/phpbin.sh
+++ tests/phpbin.sh
@@ -2,4 +2,4 @@
 
 PHP_BIN=php7.3
 
-$PHP_BIN -v $PHP_BIN >/dev/null 2>&1 || { PHP_BIN=php && echo >&2 "$PHP_BIN not found. Using default PHP binary"; php --version; }
+$PHP_BIN -v $PHP_BIN >/dev/null 2>&1 || { PHP_BIN=php && echo >&2 "Required PHP binary not found. Using default PHP binary"; php --version; }
